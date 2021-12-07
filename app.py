d7411ea37e4b84e7adb4959377b1533c406f8bc0
fradojr <fradoforumm@gmail.com>
2021-12-07 07:57:42 +0700

    coba flask murni

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, send_from_directory
 from tensorflow.keras.models import load_model
 import numpy as np
 import os
-# import cv2
+import cv2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
@@ -10,26 +10,25 @@ model = load_model('kemangi_blimbing_model_class.h5')
 
 class_dict = {0: 'Kemangi', 1: 'Belimbing'}
 
-# def predict_label(img_path):
-#     query = cv2.imread(img_path)
-#     output = query.copy()
-#     query = cv2.resize(query, (32, 32))
-#     q = []
-#     q.append(query)
-#     q = np.array(q, dtype='float') / 255.0
-#     q_pred = model.predict(q)
-#     predicted_bit = q_pred * 10
-#     print(predicted_bit)
-#     if(predicted_bit <= 1):
-#         predicted_bit = 0
-#     if(predicted_bit >= 1):
-#         predicted_bit = 1
+def predict_label(img_path):
+    query = cv2.imread(img_path)
+    output = query.copy()
+    query = cv2.resize(query, (32, 32))
+    q = []
+    q.append(query)
+    q = np.array(q, dtype='float') / 255.0
+    q_pred = model.predict(q)
+    predicted_bit = q_pred * 10
+    print(predicted_bit)
+    if(predicted_bit <= 1):
+        predicted_bit = 0
+    if(predicted_bit >= 1):
+        predicted_bit = 1
 
-#     return class_dict[predicted_bit]
+    return class_dict[predicted_bit]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
     if request.method == 'POST':
         if request.files:
             image = request.files['image']
