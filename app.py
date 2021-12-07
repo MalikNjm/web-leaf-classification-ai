3bd5a9c8cb6dbbddb0a94d67e0f3a25781323b10
fradojr <fradoforumm@gmail.com>
2021-12-07 07:32:19 +0700

    coba flask murni

--- app.py
+++ app.py
@@ -1,43 +1,44 @@
 from flask import Flask, render_template, request, send_from_directory
-from tensorflow.keras.models import load_model
-import numpy as np
-import os
-import cv2
+# from tensorflow.keras.models import load_model
+# import numpy as np
+# import os
+# import cv2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
-model = load_model('kemangi_blimbing_model_class.h5')
+# model = load_model('kemangi_blimbing_model_class.h5')
 
-class_dict = {0: 'Kemangi', 1: 'Belimbing'}
+# class_dict = {0: 'Kemangi', 1: 'Belimbing'}
 
-def predict_label(img_path):
-    query = cv2.imread(img_path)
-    output = query.copy()
-    query = cv2.resize(query, (32, 32))
-    q = []
-    q.append(query)
-    q = np.array(q, dtype='float') / 255.0
-    q_pred = model.predict(q)
-    predicted_bit = q_pred * 10
-    print(predicted_bit)
-    if(predicted_bit <= 1):
-        predicted_bit = 0
-    if(predicted_bit >= 1):
-        predicted_bit = 1
+# def predict_label(img_path):
+#     query = cv2.imread(img_path)
+#     output = query.copy()
+#     query = cv2.resize(query, (32, 32))
+#     q = []
+#     q.append(query)
+#     q = np.array(q, dtype='float') / 255.0
+#     q_pred = model.predict(q)
+#     predicted_bit = q_pred * 10
+#     print(predicted_bit)
+#     if(predicted_bit <= 1):
+#         predicted_bit = 0
+#     if(predicted_bit >= 1):
+#         predicted_bit = 1
 
-    return class_dict[predicted_bit]
+#     return class_dict[predicted_bit]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        if request.files:
-            image = request.files['image']
-            img_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-            image.save(img_path)
-            prediction = predict_label(img_path)
-            return render_template('index.html', uploaded_image=image.filename, prediction=prediction)
-
     return render_template('index.html')
+#     if request.method == 'POST':
+#         if request.files:
+#             image = request.files['image']
+#             img_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+#             image.save(img_path)
+#             prediction = predict_label(img_path)
+#             return render_template('index.html', uploaded_image=image.filename, prediction=prediction)
+
+#     return render_template('index.html')
 
 @app.route('/display/<filename>')
 def send_uploaded_image(filename=''):
